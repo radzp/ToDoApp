71e70a5c84d219d23ac90633b23bb2aa426cd2c8
Marcin Wójcik <wojcikmarcin49@gmail.com>
2023-08-24 18:34:54 +0200

    Updated Task class

--- src/main/java/com/marcin/ania/ToDoAPP/model/Task.java
+++ src/main/java/com/marcin/ania/ToDoAPP/model/Task.java
@@ -1,8 +1,48 @@
 package com.marcin.ania.ToDoAPP.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table
 public class Task {
 
+    @Id
+    private long id;
+
+    private String description;
+
+    private boolean isCompleted;
+
+    public Task() {
+    }
+
+    public Task(long id, String description, boolean isCompleted) {
+        this.id = id;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
