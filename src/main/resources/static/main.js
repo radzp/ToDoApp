26b2ee5bccc8a579db36ca33e474cb19fcce5426
radzp <anne.paczesna@gmail.com>
2023-08-26 22:42:32 +0200

    Add edit button functionality

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const newTaskInput = document.getElementById('newTask'); // New line
     let showCompletedTasks = null;
 
+    allTabBtn.classList.add('tab-btn');
+    completedTabBtn.classList.add('tab-btn');
+    activeTabBtn.classList.add('tab-btn');
+
     async function fetchAndDisplayTasks() {
         try {
             const response = await fetch('http://localhost:8080/tasks');
@@ -78,6 +82,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     }
+    
+    function changeToInputBtns(taskItem, editInput, confirmBtn, abortBtn, statusBtn, taskText, deleteBtn, editBtn){
+        taskItem.removeChild(editInput);
+        taskItem.removeChild(confirmBtn);
+        taskItem.removeChild(abortBtn);
+        taskItem.appendChild(statusBtn);
+        taskItem.appendChild(taskText);
+        taskItem.appendChild(deleteBtn);
+        taskItem.appendChild(editBtn);
+    }
 
     function createTaskElement(task) {
         const taskItem = document.createElement('li');
@@ -115,6 +129,61 @@ document.addEventListener("DOMContentLoaded", function () {
             statusTask(task);
             toggleTaskCompletion(taskItem, task);
         });
+        
+        editBtn.addEventListener('click', () => {
+            const textHolder = taskText.textContent;
+            taskItem.removeChild(statusBtn);
+            taskItem.removeChild(taskText);
+            taskItem.removeChild(deleteBtn);
+            taskItem.removeChild(editBtn);
+            const editInput = document.createElement('input');
+            editInput.classList.add('form-control', 'edit-input');
+            editInput.value = textHolder;
+            const confirmBtn = document.createElement('button');
+            confirmBtn.classList.add('confirm-btn');
+            confirmBtn.innerHTML = '<i class="fa-solid fa-check fa-beat" style="color: #48d433;"></i>';
+            const abortBtn = document.createElement('button');
+            abortBtn.classList.add('abort-btn');
+            abortBtn.innerHTML = '<i class="fa-solid fa-x fa-beat" style="color: #e32400;"></i>';
+            taskItem.appendChild(editInput);
+            taskItem.appendChild(confirmBtn);
+            taskItem.appendChild(abortBtn);
+
+            abortBtn.addEventListener('click', () => {
+                changeToInputBtns(taskItem, editInput, confirmBtn, abortBtn, statusBtn, taskText, deleteBtn, editBtn);
+            });
+
+            confirmBtn.addEventListener('click', async () =>{
+                if(editInput.value.trim() === textHolder.trim()){
+                    changeToInputBtns(taskItem, editInput, confirmBtn, abortBtn, statusBtn, taskText, deleteBtn, editBtn);
+                    return;
+                }
+                const taskId = task.id;
+                try{
+                    const response = await fetch(`http://localhost:8080/tasks/${taskId}`, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            description: editInput.value,
+                            is_completed: task.is_completed
+                        })
+                    });
+
+                    if (!response.ok) {
+                        console.error('Wystąpił błąd.');
+                        return;
+                    }
+
+                    await fetchAndDisplayTasks();
+
+                } catch(error){
+                    console.error('Wystąpił błąd:', error);
+                }
+                changeToInputBtns(taskItem, editInput, confirmBtn, abortBtn, statusBtn, taskText, deleteBtn, editBtn);
+            });
+        });
 
         deleteBtn.addEventListener('click', () => {
             const shouldDelete = confirm("Are you sure? This will delete this task.");
@@ -125,7 +194,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         return taskItem;
     }
-
+    
     function toggleTaskCompletion(taskItem, task) {
         task.completed = !task.completed;
 
