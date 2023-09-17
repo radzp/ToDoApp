2d173d040cb1eb8f2b3ef173cad348340e589447
radzp <anne.paczesna@gmail.com>
2023-09-17 20:55:00 +0200

    Add avatar get logic

---
+++ src/main/java/com/marcin/ania/ToDoAPP/model/ImageData.java
@@ -0,0 +1,30 @@
+package com.marcin.ania.ToDoAPP.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name = "ImageData")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "imagedata")
+    private byte[] imageData;
+
+    @OneToOne(mappedBy = "avatarData", fetch = FetchType.LAZY, optional = false)
+    private UserInfo userinfo;
+}
